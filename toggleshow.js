c07b9b745b62938a33b3fb504bf0f5b51887f306
paulbruzz <83400339+paulbruzz@users.noreply.github.com>
2021-05-03 15:39:31 +0200

    a

---
+++ toggleshow.js
@@ -0,0 +1,9 @@
+
+function mostranota1() {
+  var x = document.getElementById("nota1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
